51656196d7d0040579fdcc13862d87438f472141
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-19 22:06:12 +1200

    Impemented EF repository.

---
+++ DapperApp/Repository/ICompanyRepository.cs
@@ -0,0 +1,20 @@
+﻿using DapperApp.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+
+        Company Add(Company company);
+        Company Update(Company company);
+
+        void Remove(int id);
+    }
+}
