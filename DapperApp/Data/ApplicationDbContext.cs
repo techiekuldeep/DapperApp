0a03a7817ebd6676f767257973f01601db6d138d
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-19 17:46:27 +1200

    Added ApplicationDbContext class.

---
+++ DapperApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DapperApp.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
--- DapperApp/Startup.cs
+++ DapperApp/Startup.cs
@@ -1,6 +1,9 @@
+using DapperApp.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,8 @@ namespace DapperApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
